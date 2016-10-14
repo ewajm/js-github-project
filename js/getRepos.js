655c12b6597a21b65371d162f78fc6b642da7cf7
Ewa Manek <manekej@gmail.com>
2016-10-14 08:52:11 -0700

    hook provided repo call into frontend form

--- js/getRepos.js
+++ js/getRepos.js
@@ -1,8 +1,9 @@
 var apiKey = require('./../.env').apiKey;
 
-exports.getRepos = function(){
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+exports.getRepos = function(user, displayRepo){
+  $.get('https://api.github.com/users/daneden/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
+
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
