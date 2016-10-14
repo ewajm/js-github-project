baf67447c6635c4ab3c23f06ea16b95426d4218d
Ewa Manek <manekej@gmail.com>
2016-10-14 09:05:22 -0700

    Add function to display username and repo results when form is submitted

--- js/getRepos.js
+++ js/getRepos.js
@@ -3,7 +3,14 @@ var apiKey = require('./../.env').apiKey;
 exports.getRepos = function(user, displayRepo){
   $.get('https://api.github.com/users/daneden/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
-
+    for(var i = 0; i < response.length; i++){
+      var name = response[i].name;
+      var url = response[i].html_url;
+      var description = response[i].description ? response[i].description : "No description provided";
+      var language = response[i].language;
+      var homepage = response[i].homepage;
+      displayRepo(name, url, description, language, homepage);
+    }
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
--- js/search-interface.js
+++ js/search-interface.js
@@ -1,13 +1,13 @@
 var getRepos = require('./../js/getRepos.js').getRepos;
 
 var displayUser = function(username){
-  $("#results h2").html("<a href='https://github.com/" + username "'>" + username + "'s Repos'</a>");
-}
+  $("#results h2").html("<a href='https://github.com/" + username + "'>" + username + "'s Repos</a>");
+};
 
 var displayRepo = function(name, url, description, language, homepage){
   var repoString = "<li><a href='" + url + "'>" + name + "</a> - " + description + "</li>";
   $("#results ul").append(repoString);
-}
+};
 
 $(document).ready(function(){
   $("#search-form").submit(function(event){
